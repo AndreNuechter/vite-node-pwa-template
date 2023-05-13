87346146aeae4843c1a5ab910ba4567b540d9d09
Andre Nuechter <andre_nuechter@hotmail.de>
2023-05-13 14:54:30 +0200

    Tweaked deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-index_has_changes=false;
+function main() {
+    local index_is_dirty=false;
 
-if [[ `git status --porcelain` ]];
-    then index_has_changes=true;
-fi
+    if [[ `git status --porcelain` ]];
+        then index_is_dirty=true;
+    fi
 
-git stash --include-untracked;
+    git stash --include-untracked;
 
-if [[ "$index_has_changes" = true ]];
-    then git stash pop stash@{0};
-fi
+    npm run build;
+    git add docs/.;
+    git commit --amend --no-edit;
+    # git push --all;
+
+    if [[ "$index_is_dirty" = true ]];
+        then git stash pop stash@{0};
+    fi
+}
+
+main
